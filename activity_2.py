907e991d2eb4427043d6ed97928f8781a38bb0ef
Benidict Dulce <benidict@metromart.com>
2025-09-08 10:31:11 +0800

    activity2: done with activity 2 - B.

--- activity_2.py
+++ activity_2.py
@@ -3,3 +3,10 @@ name = "Benidict"
 message = f"Hello {name}, would you like to learn some Python today?"
 print(message)
 
+print("\n")
+print("Activity 2: B")
+name = "bEnidict dUlce"
+print(f"actual name: {name}")
+print(f"lower case: {name.lower()}")
+print(f"upper case: {name.upper()}")
+print(f"title case: {name.title()}")
