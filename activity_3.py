a626b6500ede12d8be8d8eb14a90d7661e8b67e3
Benidict Dulce <benidict@metromart.com>
2025-09-09 19:39:24 +0800

    activity: #4 use comment to the activity #3.

--- activity_3.py
+++ activity_3.py
@@ -1,9 +1,15 @@
+# this is for addition
 add = 5 + 3
+# this is for substraction
 sub = 10 - 2
+# this is for division
 div = 8 / 1
+# this is for multiplication
 mul = 2 ** 3
 
+# print the four programming computation
 print(f"add:{add}\nsub:{sub}\ndiv:{div}\nmul:{mul}")
 
+# 100,000,000 is my goal
 FAVORITE_NUMNER = 100_000_000
 print(f"My goal is {FAVORITE_NUMNER}")
