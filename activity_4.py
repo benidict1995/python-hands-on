248411f91e532166d378661730f8381f19b87a34
Benidict Dulce <benidict@metromart.com>
2025-09-09 19:56:19 +0800

    activity: done with activity #4 which is using lists

---
+++ activity_4.py
@@ -0,0 +1,9 @@
+names = ['ben', 'nina', 'nino']
+print(f"name:{names[0].title()}")
+print(f"name:{names[1].title()}")
+print(f"name:{names[2].title()}")
+
+
+transportation = ['motory', 'car', 'airplane']
+for transport in transportation:
+    print(f"My first bicycle was a {transport.title()}.")
