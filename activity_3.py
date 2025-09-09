8bf3c102fd356748bbe12d87f49e9649adc54cc5
Benidict Dulce <benidict@metromart.com>
2025-09-09 19:36:22 +0800

    activity: #3 done to try the number activity.

---
+++ activity_3.py
@@ -0,0 +1,9 @@
+add = 5 + 3
+sub = 10 - 2
+div = 8 / 1
+mul = 2 ** 3
+
+print(f"add:{add}\nsub:{sub}\ndiv:{div}\nmul:{mul}")
+
+FAVORITE_NUMNER = 100_000_000
+print(f"My goal is {FAVORITE_NUMNER}")
---
+++ num.py
@@ -0,0 +1,7 @@
+universe_age = 11_000_000_000
+print(universe_age)
+
+x, y ,z = 1, 2, "test numbers"
+print(f"x = {x} \ny = {y} \nz = {z}")
+
+MAX_CONNECTIONS = 1000
