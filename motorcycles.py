58841b4ff0194220bde9420170f4d5118cabd8e0
Benidict Dulce <benidict@metromart.com>
2025-09-14 22:37:45 +0800

    lec: dynamic lists.

---
+++ motorcycles.py
@@ -0,0 +1,27 @@
+#UPDATE ITEM
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#motorcycles[0] = 'ducati'
+
+#ADD ITEM
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(f"Old list: {motorcycles}")
+#motorcycles.append('ducati')
+#print(f"\nNew list: {motorcycles}")
+
+#INSERTING ITEM WITH POSITION
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#motorcycles.insert(0, 'ducati')
+#print(motorcycles)
+
+#REMOVE ITEM
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+#del motorcycles[1] #DEL
+#print(motorcycles)
+#popped_motorcycles = motorcycles.pop() #POP
+#print(popped_motorcycles)
+#popped_motorcycles = motorcycles.pop(2) #POP WITH POS
+#print(popped_motorcycles.title())
+
+motorcycles.remove('yamaha') #Removing an Item by Value
+print(motorcycles)
