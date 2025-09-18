dbce76b2102d5dbe2c2871ff723b96a32c1ac21c
Benidict Dulce <benidict@metromart.com>
2025-09-18 23:22:34 +0800

    activity: done with activity #10 regarding on the immutable list using tuples.

---
+++ activity_10.py
@@ -0,0 +1,9 @@
+menu = ('Rice - 25php', 'Hotdog - 10php', 'Egg - 12php', 'Chicken - 50php', 'Softdrinks - 20php')
+print("Menu")
+for food in menu:
+    print(food)
+
+print("\nNew Menu")
+menu = ('Soup - 15php', 'Adobo - 30php', 'Egg - 12php', 'Chicken - 50php', 'Softdrinks - 20php')
+for food in menu:
+    print(food)
