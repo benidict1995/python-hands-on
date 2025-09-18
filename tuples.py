5d06cdfbfebda32c7b8d2747928db35aef559865
Benidict Dulce <benidict@metromart.com>
2025-09-18 23:17:45 +0800

    lec: tuples modified dimensions.

--- tuples.py
+++ tuples.py
@@ -3,3 +3,13 @@ count = 0
 for dimension in dimensions:
     print(f"dimension: {dimensions[count]}")
     count += 1
+
+original_dimensions = (200,50)
+print("Original dimensions:")
+for dimension in original_dimensions:
+    print(dimension)
+
+original_dimensions = (400, 100)
+print("Modified dimensions:")
+for dimension in original_dimensions:
+    print(dimension)
