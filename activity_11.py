32e3650299aafc046f3f27a6a4b5d1b24d138769
Benidict Dulce <benidict@metromart.com>
2025-09-20 22:10:22 +0800

    activity: activity 11 done

---
+++ activity_11.py
@@ -0,0 +1,13 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+banned_users = ['ben', 'nina', 'jeje', 'reylan']
+user_0 = 'ben'
+user_1 = 'nino'
+print("Is ben is banned ?")
+print(user_0 in banned_users)
+print("Is nino is banned ?")
+print(user_1 in banned_users)
