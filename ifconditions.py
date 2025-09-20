6d065c0afcb4e2785dcf64f5d8370e9f4852247e
Benidict Dulce <benidict@metromart.com>
2025-09-20 22:06:57 +0800

    lec: how to use in or not in if condition

--- ifconditions.py
+++ ifconditions.py
@@ -4,3 +4,21 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title()) 
+
+
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+
+toppings = ['mushrooms', 'onions', 'pineapple']
+isExists = 'mushrooms' in toppings
+print(f"isExists:{isExists}")
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(f"{user.title()}, is not banned!")
+else :
+    print("The user is Banned!")
