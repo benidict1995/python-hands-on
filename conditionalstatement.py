4c713a0520947e412e41efe9402700142071e374
Benidict Dulce <benidict@metromart.com>
2025-09-23 14:28:32 +0800

    lec: condition to check the items inside array.

--- conditionalstatement.py
+++ conditionalstatement.py
@@ -11,3 +11,20 @@ elif ride_age < 18:
     print("Your admission cost is $25.")
 else: 
     print("Your admission cost is $40.")
+
+price = 0
+age_price = 19
+if age_price < 4:
+    price = 0
+elif age_price < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission cost base on the age is ${price}.")
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Mushrooms is already added.")
+if 'pepperoni' is not requested_toppings:
+    print("Adding pepperoni to the list.")
