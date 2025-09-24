f08c395c6c7776b1e6bde08d19468ccb17a9b513
Benidict Dulce <benidict@metromart.com>
2025-09-24 20:55:50 +0800

    activity: done with activity 14

---
+++ activity_14.py
@@ -0,0 +1,15 @@
+#1
+person = {'first_name': 'benidict', 'last_name': 'dulce', 'age': 1, 'city': 'Mundo City'}
+print(f"First name:{person['first_name'].title()}")
+print(f"Last name:{person['last_name'].title()}")
+print(f"Age:{person['age']}")
+print(f"City:{person['city']}")
+
+print("\n")
+#2
+favorite_number = {'benidict': 2, 'nina': 100, 'rd': 50, 'leo': 10, 'reyland': 5}
+print(f"Hi Benidict, your favorite number is {'benidict'}")
+print(f"Hi Nina, your favorite number is {'nina'}")
+print(f"Hi RD, your favorite number is {'rd'}")
+print(f"Hi Leo, your favorite number is {'leo'}")
+print(f"Hi Reyland, your favorite number is {'reyland'}")
