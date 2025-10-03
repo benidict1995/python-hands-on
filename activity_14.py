28d67a00e5a58e4b438ad5ba3d4555390c1077b6
Benidict Dulce <benidict@metromart.com>
2025-10-03 21:48:55 +0800

    activity: done #14

--- activity_14.py
+++ activity_14.py
@@ -8,8 +8,15 @@ print(f"City:{person['city']}")
 print("\n")
 #2
 favorite_number = {'benidict': 2, 'nina': 100, 'rd': 50, 'leo': 10, 'reyland': 5}
-print(f"Hi Benidict, your favorite number is {'benidict'}")
-print(f"Hi Nina, your favorite number is {'nina'}")
-print(f"Hi RD, your favorite number is {'rd'}")
-print(f"Hi Leo, your favorite number is {'leo'}")
-print(f"Hi Reyland, your favorite number is {'reyland'}")
+print(f"Hi Benidict, your favorite number is {favorite_number['benidict']}")
+print(f"Hi Nina, your favorite number is {favorite_number['nina']}")
+print(f"Hi RD, your favorite number is {favorite_number['rd']}")
+print(f"Hi Leo, your favorite number is {favorite_number['leo']}")
+print(f"Hi Reyland, your favorite number is {favorite_number['reyland']}")
+
+print("\n")
+#3
+glossary = {'kotlin': 'programming language for Android development', 'xcode': 'programming language for iOS development',
+            'php': 'programming language for Web development', 'c#': 'programming language for Web development', 'python': 'programming for Web development.'}
+for gloss in glossary:
+    print(f"{gloss}: ${glossary[gloss]}\n")
