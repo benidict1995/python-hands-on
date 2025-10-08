f93b8eda915aed1093ae812adc6bae742d26b891
Benidict Dulce <benidict@metromart.com>
2025-10-08 10:45:04 +0800

    activity: done with activity 20

---
+++ activity_20.py
@@ -0,0 +1,12 @@
+#1
+def display_message():
+   print("I've learned a lot on this python books") 
+
+display_message()
+
+#2
+print("\n")
+def favorite_books(book_name):
+   print(f"My favorite book is {book_name.title()}")
+
+favorite_books("super book")
