e33156f3f9a3d4db13f08e8ed62149018a914b08
Benidict Dulce <benidict@metromart.com>
2026-01-04 20:45:58 +0800

    lec: refactor code

--- remember_me.py
+++ remember_me.py
@@ -2,15 +2,20 @@ from pathlib import Path
 import json
 
 
-path = Path('file/username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("what is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
+def greet_user():
+    """Greet the user by name."""
+    path = Path('file/username.json')
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("what is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
 
-    print(f"We'll remember you when you come back, {username}!\n")
+        print(f"We'll remember you when you come back, {username}!\n")
+
+
+greet_user()
 
